dc614c8ce879b2873649aa05c930cdfc0463a327
Shraddha <Shraddha@ShRaddha>
2017-10-31 01:01:44 -0700

    Added code for Update , delete and add messages.

--- messenger/src/main/java/com/ws/restful/messenger/resources/MessageResource.java
+++ messenger/src/main/java/com/ws/restful/messenger/resources/MessageResource.java
@@ -2,8 +2,13 @@ package com.ws.restful.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -11,14 +16,42 @@ import com.ws.restful.messenger.model.Message;
 import com.ws.restful.messenger.service.MessageService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 	
 	MessageService ms = new MessageService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
 	public List<Message> getMessages(){
 		return ms.getAllMessages();
 	}
-
+	
+	@GET
+	@Path("/{messageId}")
+	public Message getMessage(@PathParam("messageId") long messageId){
+		return ms.getMessage(messageId);
+	
+	}
+	
+	@POST
+	public Message addMessage(Message message){
+		return ms.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long messageId , Message message){
+		message.setId(messageId);
+		return ms.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long messageId){
+		ms.removeMessage(messageId);
+	}
+	
+	
+	
 }
