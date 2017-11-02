8a2351440a5f15f249607d2921985d90b088f8d4
Shraddha Apharande <ApharandeShraddha@users.noreply.github.com>
2017-11-02 01:41:53 -0700

    Added Exception Handling code

---
+++ messenger/src/main/java/com/ws/restful/messenger/exception/DataNotFoundException.java
@@ -0,0 +1,14 @@
+package com.ws.restful.messenger.exception;
+
+public class DataNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public DataNotFoundException(String message) {
+		super(message);
+	}
+
+}
