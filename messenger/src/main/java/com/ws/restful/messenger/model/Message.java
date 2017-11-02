193b9c8b10d0d3ab3dbd5dbb289e6cd6fe959384
Shraddha Apharande <ApharandeShraddha@users.noreply.github.com>
2017-11-02 01:44:15 -0700

    Added Links and ErrorMessage POJO Class

--- messenger/src/main/java/com/ws/restful/messenger/model/Message.java
+++ messenger/src/main/java/com/ws/restful/messenger/model/Message.java
@@ -1,7 +1,12 @@
 package com.ws.restful.messenger.model;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 @XmlRootElement
 public class Message {
@@ -10,6 +15,9 @@ public class Message {
 	private String message;
 	private Date created;
 	private String author;
+	
+	private Map<Long, Comment> comments = new HashMap<>();
+	private List<Link> links = new ArrayList<>();
 
 	public Message() {
 
@@ -53,5 +61,31 @@ public class Message {
 	public void setAuthor(String author) {
 		this.author = author;
 	}
+	
+	//Ignore comments when accessing message object as XML
+	@XmlTransient
+	public Map<Long, Comment> getComments() {
+		return comments;
+	}
+
+	public void setComments(Map<Long, Comment> comments) {
+		this.comments = comments;
+	}
+
+	public List<Link> getLinks() {
+		return links;
+	}
+
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
+	
+	public void addLink(String url, String rel) {
+		Link link = new Link();
+		link.setLink(url);
+		link.setRel(rel);
+		links.add(link);
+	}
+    
 
 }
